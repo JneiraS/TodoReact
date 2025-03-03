b7a58ba8d6acf26d2697370941d4939d2fc91060
NeiraJ <johnny.neira@gmail.com>
2025-03-03 11:22:32 +0100

    feat: implémenter une liste de tâches avec API mock
    * Supprimer le composant Greeting et les logos Vite/React
    * Remplacer useState par une liste de tâches statique
    * Simplifier TaskCreator en supprimant l'héritage abstrait
    * Ajouter rendu des tâches avec map()
    * Mettre à jour l'interface utilisateur pour afficher la liste

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,46 @@
 
 import './App.css'
+import { useState, useEffect } from "react";
 import { TaskCreator } from './entities/taskCreator'
 import { Task } from './entities/task'
 
 function App() {
+
+
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+
+  // Appliquer le thème au chargement
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  // Fonction pour basculer le mode
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
 
   const apiMock =[
     {id:1, title: "Apprendre React"},
     {id:2, title: "Créer une Todo List"},
     {id:3, title: "Boire un café"}
   ]
-
-  // const newTask = new TaskCreator().factoryMethod()
-  //newTask.setId(15);
-  //newTask.setTitle("Apprender React")
   
   const tasks: Task[] = []
-  // Correction de la boucle pour créer les tâches
+
   apiMock?.forEach((item: { id: number, title: string }) => {
     const task = new TaskCreator().factoryMethod(item.id, item.title)
-
     tasks.push(task)
   })
   
   return (
     <>
+        <div>
+      <h1>Bienvenue sur mon App</h1>
+      <button className="theme-switch" onClick={toggleTheme}>
+        {theme === "light" ? "🌙 Mode sombre" : "☀️ Mode clair"}
+      </button>
+    </div>
       <div>
         <h1>ToDo List</h1> 
         <ul>
